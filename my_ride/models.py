94892bba022d7ca0a7dc8883670418948e185b3c
KD3821 <80853241+KD3821@users.noreply.github.com>
2022-08-06 00:18:24 +0300

    1st commit

---
+++ my_ride/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.db.models import DateField, DateTimeField, CharField, IntegerField, BooleanField, ForeignKey, TextField
+
+
+class Shift(models.Model):
+    date = DateField()
+
+    def __str__(self):
+        return self.date
+
+
+class Car(models.Model):
+    plate = CharField(max_length=200)
+    date = ForeignKey(Shift, on_delete=models.CASCADE)
+    income = IntegerField(default=0)
+    rides = IntegerField(default=0)
+    fuel = IntegerField(default=0)
+    wash = IntegerField(default=0)
+    fix = IntegerField(default=0)
+    service = IntegerField(default=0)
+    other = IntegerField(default=0)
+
+    def __str__(self):
+        return self.plate
+
+
+class Driver(models.Model):
+    name = CharField(max_length=200)
+    salary = IntegerField(default=0)
+    tips = IntegerField(default=0)
+    costs = IntegerField(default=0)
+    owns = IntegerField(default=0)
+    fines = IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Ride(models.Model):
+    number = CharField(max_length=100)
+    driver = ForeignKey(Driver, on_delete=models.CASCADE)
+    car = ForeignKey(Car, on_delete=models.CASCADE)
+    shift = ForeignKey(Shift, on_delete=models.CASCADE)
+    price = IntegerField(default=0)
+    cash = BooleanField(default=False)
+    toll = IntegerField(default=0)
+    save_tax = BooleanField(default=False)
+    extra_tax = IntegerField(default=0)
+    comment = TextField()
+
+    def __str__(self):
+        return self.number
+
