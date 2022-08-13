e932298c3a726ffe5e0b699f085de59fbe2c196b
KD3821 <80853241+KD3821@users.noreply.github.com>
2022-08-13 03:31:46 +0300

    base b_logic done

--- my_ride/urls.py
+++ my_ride/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import enter_ride, show_rides, CarShift
+from .views import enter_ride, show_rides, CarShift, show_detail, edit_ride
 
 urlpatterns = [
     path('add/', enter_ride, name="new_ride"),
     path('added/', enter_ride, name="ride_added"),
     path('list/', show_rides, name="rides_all"),
     path('totaldaycar/', CarShift.as_view(), name="total_day_car"),
+    path('detail/(?P<number>[0-9]+)/', show_detail, name="ride_detail"),
+    path('edit/(?P<number>[0-9]+)/', edit_ride, name="ride_change"),
 ]
--- my_ride/views.py
+++ my_ride/views.py
@@ -6,23 +6,6 @@ from django.contrib import messages
 from total import GrossCar
 
 
-class CarShift(View):
-    def get(self, request):
-        form = TotalCarForm()
-        return render(request, 'total/totalcar.html', {'form': form})
-
-    def post(self, request):
-        form = TotalCarForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            shift, car = data['shift'], data['car']
-            shift = str(shift)
-            gross = GrossCar()
-            raw_result = gross.rides_day(shift)
-            result = gross.rides_day_car(shift, car)
-            return render(request, 'total/totalcar.html', {'result': result, 'form': form})
-
-
 def enter_ride(request):
     ride = Ride()
     form = RideForm(request.POST or None)
@@ -43,6 +26,7 @@ def enter_ride(request):
         return render(request, 'ride_done.html', {'ride': ride})
     return render(request, 'add_ride.html', {'form': form})
 
+
 def show_rides(request):
     rides = Ride.objects.all()
     for ride in rides:
@@ -51,3 +35,55 @@ def show_rides(request):
         else:
             ride.cash = ride.price
     return render(request, 'ride_list.html', {'rides': rides})
+
+def show_detail(request, number):
+    ride = Ride.objects.get(number=number)
+    return render(request, 'ride_detail.html', {'ride': ride})
+
+def edit_ride(request, number):
+    ride = Ride.objects.get(number=number)
+    form = RideForm(request.POST or None, initial={
+        'number': ride.number,
+        'car': ride.car,
+        'driver': ride.driver,
+        'shift': ride.shift,
+        'price': ride.price,
+        'cash': ride.cash,
+        'toll': ride.toll,
+        'save_tax': ride.save_tax,
+        'extra_tax': ride.extra_tax,
+        'comment': ride.comment
+    })
+    if form.is_valid():
+        data = form.cleaned_data
+        ride.number = data.get('number')
+        ride.car = data.get('car')
+        ride.driver = data.get('driver')
+        ride.shift = data.get('shift')
+        ride.price = data.get('price')
+        ride.cash = data.get('cash')
+        ride.toll = data.get('toll')
+        ride.save_tax = data.get('save_tax')
+        ride.extra_tax = data.get('extra_tax')
+        ride.comment = data.get('comment')
+        ride.save()
+        messages.success(request, 'Поездка изменена!')
+        return render(request, 'ride_detail.html', {'ride': ride})
+    return render(request, 'ride_change.html', {'form': form, 'ride': ride})
+
+
+class CarShift(View):
+    def get(self, request):
+        form = TotalCarForm()
+        return render(request, 'total/totalcar.html', {'form': form})
+
+    def post(self, request):
+        form = TotalCarForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            shift, car = data['shift'], data['car']
+            shift = str(shift)
+            gross = GrossCar()
+            # raw_result = gross.rides_day(shift)
+            result = gross.rides_day_car(shift, car)
+            return render(request, 'total/totalcar.html', {'result': result, 'form': form})
